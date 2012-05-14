f746c9bf529bca0fcdff0767b6d550a143a73965
bigbiff bigbiff <bigbiff@teamw.in>
2012-05-14 12:01:03 -0400

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,53 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/samsung/i717/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+
+TARGET_BOARD_PLATFORM := msm8660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+BOARD_USES_ADRENO_200 := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := i717
+
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_watchdog.appsbark=1 loglevel=4 
+BOARD_KERNEL_BASE := 0x48000000
+BOARD_PAGE_SIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x40000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x80200000
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_FORCE_RAMDISK_ADDRESS := 0x49400000
+
+TARGET_PREBUILT_KERNEL := device/samsung/i717/kernel
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := "roboto_15x24.h"
+TARGET_RECOVERY_INITRC := device/samsung/i717/init.recovery.rc
+
+DEVICE_RESOLUTION := 800x1280
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_INTERNAL_STORAGE_PATH := "/emmc"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
+TW_EXTERNAL_STORAGE_PATH := "/sdc"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdc"
+
