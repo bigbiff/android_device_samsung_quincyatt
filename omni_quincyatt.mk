859c6482034a775a58daa40e965146f7873e2e29
bigbiff bigbiff <bigbiff@teamw.in>
2014-07-02 20:00:00 -0400

    modify for omni

---
+++ omni_quincyatt.mk
@@ -0,0 +1,12 @@
+# Release name
+PRODUCT_RELEASE_NAME := quincyatt
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/quincyatt/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := quincyatt
+PRODUCT_NAME := omni_quincyatt
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := quincyatt
+PRODUCT_MANUFACTURER := samsung
